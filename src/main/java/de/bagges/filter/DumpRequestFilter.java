03c5ec6065feb62851013d9dac65d20f2be35658
Markus Backes <markus.backes@gmail.com>
2015-07-21 20:27:50 +0200

    added feature to disable filter via contex-param
     * dump.request and dump.response

--- src/main/java/de/bagges/filter/DumpRequestFilter.java
+++ src/main/java/de/bagges/filter/DumpRequestFilter.java
@@ -2,8 +2,11 @@ package de.bagges.filter;
 
 import de.bagges.filter.shared.Constants;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
 import java.io.ByteArrayInputStream;
@@ -18,29 +21,42 @@ import java.util.logging.Logger;
 public class DumpRequestFilter implements ContainerRequestFilter {
 
     private static final Logger LOG = Logger.getLogger(DumpRequestFilter.class.getName());
+    private boolean ENABLED = true;
+
+    @Context
+    private HttpServletRequest request;
+
+    @PostConstruct
+    public void init() {
+        String enabled = request.getServletContext().getInitParameter(Constants.DUMP_REQUEST_CONFIG);
+        if(enabled != null && !enabled.isEmpty()) {
+            ENABLED = enabled.equalsIgnoreCase("TRUE");
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-        StringBuilder builder = new StringBuilder();
-        builder.append("\n-----HTTP REQUEST ---------------------\n");
-        builder.append("------------ HTTP REQUEST ------\n");
-        builder.append(String.format(Constants.HEADER_TEMPLATE, "Request URL", containerRequestContext.getUriInfo().getAbsolutePath()));
-        builder.append(String.format(Constants.HEADER_TEMPLATE, "Request Method", containerRequestContext.getMethod()));
-        builder.append("------------ HTTP REQUEST HEADER ------\n");
-        containerRequestContext.getHeaders().forEach((k, v) -> builder.append(String.format(Constants.HEADER_TEMPLATE, k, v)));
-        builder.append("------------ HTTP REQUEST BODY --------\n");
-        if(!containerRequestContext.getMediaType().equals(MediaType.APPLICATION_OCTET_STREAM)) {
-            try(Scanner sc = new java.util.Scanner(containerRequestContext.getEntityStream()).useDelimiter("\\A")) {
-                if (sc.hasNext()) {
-                    String body = sc.next();
-                    containerRequestContext.setEntityStream(new ByteArrayInputStream(body.getBytes()));
-                    builder.append(body);
+        if(ENABLED) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\n-----HTTP REQUEST ---------------------\n");
+            builder.append("------------ HTTP REQUEST ------\n");
+            builder.append(String.format(Constants.HEADER_TEMPLATE, "Request URL", containerRequestContext.getUriInfo().getAbsolutePath()));
+            builder.append(String.format(Constants.HEADER_TEMPLATE, "Request Method", containerRequestContext.getMethod()));
+            builder.append("------------ HTTP REQUEST HEADER ------\n");
+            containerRequestContext.getHeaders().forEach((k, v) -> builder.append(String.format(Constants.HEADER_TEMPLATE, k, v)));
+            builder.append("------------ HTTP REQUEST BODY --------\n");
+            if (containerRequestContext.getMediaType() == null || !containerRequestContext.getMediaType().equals(MediaType.APPLICATION_OCTET_STREAM)) {
+                try (Scanner sc = new java.util.Scanner(containerRequestContext.getEntityStream()).useDelimiter("\\A")) {
+                    if (sc.hasNext()) {
+                        String body = sc.next();
+                        containerRequestContext.setEntityStream(new ByteArrayInputStream(body.getBytes()));
+                        builder.append(body);
+                    }
                 }
+            } else {
+                builder.append(Constants.SKIPPING_BODY);
             }
-        } else {
-            builder.append(Constants.SKIPPING_BODY);
+            LOG.info(builder.toString());
         }
-
-        LOG.info(builder.toString());
     }
 }
--- src/main/java/de/bagges/filter/DumpResponseFilter.java
+++ src/main/java/de/bagges/filter/DumpResponseFilter.java
@@ -2,9 +2,12 @@ package de.bagges.filter;
 
 import de.bagges.filter.shared.Constants;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
@@ -17,21 +20,35 @@ import java.util.logging.Logger;
 public class DumpResponseFilter implements ContainerResponseFilter {
 
     private static final Logger LOG = Logger.getLogger(DumpResponseFilter.class.getName());
+    private boolean ENABLED = true;
+
+    @Context
+    private HttpServletRequest request;
+
+    @PostConstruct
+    public void init() {
+        String enabled = request.getServletContext().getInitParameter(Constants.DUMP_RESPONSE_CONFIG);
+        if(enabled != null && !enabled.isEmpty()) {
+            ENABLED = enabled.equalsIgnoreCase("TRUE");
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
-        StringBuilder builder = new StringBuilder();
-        builder.append("\n-----HTTP RESPONSE ---------------------\n");
-        builder.append("------------ HTTP RESPONSE ------\n");
-        builder.append(String.format(Constants.HEADER_TEMPLATE, "Status", containerResponseContext.getStatus()));
-        builder.append("------------ HTTP RESPONSE HEADER ------\n");
-        containerRequestContext.getHeaders().forEach((k, v) -> builder.append(String.format(Constants.HEADER_TEMPLATE, k, v)));
-        builder.append("------------ HTTP RESPONSE BODY --------\n");
-        if(!containerResponseContext.getMediaType().equals(MediaType.APPLICATION_OCTET_STREAM)) {
-            builder.append(containerResponseContext.getEntity());
-        } else {
-            builder.append(Constants.SKIPPING_BODY);
+        if(ENABLED) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\n-----HTTP RESPONSE ---------------------\n");
+            builder.append("------------ HTTP RESPONSE ------\n");
+            builder.append(String.format(Constants.HEADER_TEMPLATE, "Status", containerResponseContext.getStatus()));
+            builder.append("------------ HTTP RESPONSE HEADER ------\n");
+            containerRequestContext.getHeaders().forEach((k, v) -> builder.append(String.format(Constants.HEADER_TEMPLATE, k, v)));
+            builder.append("------------ HTTP RESPONSE BODY --------\n");
+            if (containerResponseContext.getMediaType() == null || !containerResponseContext.getMediaType().equals(MediaType.APPLICATION_OCTET_STREAM)) {
+                builder.append(containerResponseContext.getEntity());
+            } else {
+                builder.append(Constants.SKIPPING_BODY);
+            }
+            LOG.info(builder.toString());
         }
-        LOG.info(builder.toString());
     }
 }
